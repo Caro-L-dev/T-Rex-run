2a2c14d9e87b7c234bc1c5e8648b9e8865282af4
Caro-L-dev <liardcar@gmail.com>
2021-02-03 12:43:41 +0100

    :sparkles: create jump function

--- app.js
+++ app.js
@@ -4,8 +4,28 @@ document.addEventListener('DOMContentLoaded', () => {
     function control(event) {
         // keycode - keyboard touch (spacebar)
         if (event.keyCode === 32) {
-        console.log('pressed');
+        jump();
         }
     }
     document.addEventListener('keyup', control);
+
+    function jump() {
+        let position = 0;
+        let timerId = setInterval(function () {
+            // move down
+            if (position === 150) {
+                clearInterval(timerId);
+                console.log('down');
+                let downTimerId = setInterval(function () {
+                    position -=30;
+                    dino.style.bottom = position + 'px';
+                },20);
+            }
+
+            // move up
+            console.log('up');
+            position +=30;
+            dino.style.bottom = position + 'px';
+        },20);
+    }
 });
